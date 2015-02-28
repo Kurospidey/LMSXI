19159be7c8d6abfd13d8ad470390dcd2e105a776
Kurospidey <fran.rives@hotmail.com>
2015-02-28 23:59:18 +0100

    versión para presentación

--- scripts/script1.js
+++ scripts/script1.js
@@ -17,11 +17,11 @@
 "use strict";
 var re = /img\/galeria\/imagen..\.jpg/, // expresión regular para extraer la ruta relativa de los <img>.
     imagesRoutes = [], // array para poner las rutas relativas de los <img> de la galería.
-    canvasOpeners = document.getElementsByClassName('ampliar'), // array con los enlaces de los <img> que inician el slide.
+    canvasOpeners = document.getElementsByClassName('ampliar'), // array con los enlaces de los <img> que inician el slider.
 
     i, // contador que usan todos los for.
-    canvas = document.getElementById('marco').parentElement, // caja que contiene el slide.
-    placeholders = document.getElementById('marco').getElementsByTagName('div'), // array con los 3 <div> que forman el slide.
+    canvas = document.getElementById('marco').parentElement, // caja que contiene el slider.
+    placeholders = document.getElementById('marco').getElementsByTagName('div'), // array con los 3 <div> que forman el slider.
 
     // Estas tres variables guardan qué placeholders se encuentran posicionados a la izquierda, derecha, y centro (el único visible del slide).
     // Aunque se inicialicen con el mismo orden con el que aparecen en el código html, conforme se mueva el slider esto ya no será así.
@@ -29,9 +29,9 @@ var re = /img\/galeria\/imagen..\.jpg/, // expresión regular para extraer la ru
     phCtro = placeholders[1],
     phDcha = placeholders[2],
     currPhotoIndex,
-    nav1Links = document.getElementById('slideNav1').getElementsByTagName('a'), // array con los enlaces de navegación del slide.
+    nav1Links = document.getElementById('slideNav1').getElementsByTagName('a'), // array con los enlaces de navegación del slider.
     nav2Links = document.getElementById('slideNav2').getElementsByTagName('a'),
-    slideCloseButton = document.getElementById('marco').parentElement.getElementsByTagName('a')[document.getElementById('marco').parentElement.getElementsByTagName('a').length - 1], // botón para cerrar el slide.
+    slideCloseButton = document.getElementById('marco').parentElement.getElementsByTagName('a')[document.getElementById('marco').parentElement.getElementsByTagName('a').length - 1], // botón para cerrar el slider.
     on = 0,
     state;
 
@@ -47,13 +47,14 @@ function relRoutes() {
     }
 }
 
+// Carga las imágenes de previsualización que correspondan a cada botón.
 function loadPreview() {
     for (i = 0; i < imagesRoutes.length; i++) {
         nav2Links[i + 12].style.backgroundImage = 'url(' + imagesRoutes[i] + ')';
     }
 }
 
-// Muestra y oculta alternativamente la caja del slide (coge como parámetro el evento que la ha iniciado).
+// Muestra y oculta alternativamente la caja del slider (coge como parámetro el evento que la ha iniciado).
 function switchCanvas(e) {
     if (window.getComputedStyle(canvas).display === "none") { // si está invisible, 1º le asigna las fotos iniciales y después la hace visible.
         loadInitialPhoto(e);
@@ -67,12 +68,12 @@ function switchCanvas(e) {
     }
 }
 
-// Asigna la foto inicial que se verá en el marco del slide y las que están a su izquierda y a su derecha.
+// Asigna la foto inicial que se verá en el marco del slider y las que están a su izquierda y a su derecha.
 function loadInitialPhoto(e) {
-    // Extrae la ruta relativa del enlace de imagen que abrió el slide (match devuelve un array aunque solo haya una coincidencia).
+    // Extrae la ruta relativa del enlace de imagen que abrió el slider (match devuelve un array aunque solo haya una coincidencia).
     var currPhoto = e.target.parentElement.parentElement.getElementsByTagName('img')[0].src.match(re)[0];
 
-    // Examina las rutas relativas de todas las imágenes de la galería hasta encontrar una coincidencia con la imagen del enlace que abrió el slide.
+    // Examina las rutas relativas de todas las imágenes de la galería hasta encontrar una coincidencia con la imagen del enlace que abrió el slider.
     // Después configura la imagen de fondo de cada placeholder acorde a su posición.
     for (i = 0; i < imagesRoutes.length; i++) {
         if (currPhoto === imagesRoutes[i]) {
@@ -123,6 +124,7 @@ function triggerSlider(e) {
     }
 }
 
+/*loadLeftPhoto y loadRightPhoto cargan las fotos que se verán cuando pulsemos los botones de retroceso, avance, o le demos al play y cargen automáticamente.*/
 function loadLeftPhoto() {
     if (imagesRoutes[currPhotoIndex] === imagesRoutes[0]) {
         phIzq.style.backgroundImage = 'url(' + imagesRoutes[imagesRoutes.length - 1] + ')';
@@ -143,6 +145,7 @@ function loadRightPhoto() {
     }
 }
 
+/*Activa el slider automático.*/
 function toggleSlider() {
     if (on === 0) {
         on = 1;
@@ -160,6 +163,7 @@ function toggleSlider() {
     }
 }
 
+/*Activa la imagen de previsualización correspondiente.*/
 function activPreview(i) {
     return function () {
         if (i != currPhotoIndex) {
@@ -168,12 +172,14 @@ function activPreview(i) {
     };
 }
 
+/*Desactiva la imagen de previsualización correspondiente.*/
 function deactivPreview(i) {
     return function () {
         nav2Links[i + 12].style.display = "none";
     };
 }
 
+/*Carga en el marco del slider la imagen correspondiente al botón que hayamos clicado.*/
 function loadPreviewPhoto(i) {
     return function () {
         if (i != currPhotoIndex) {
@@ -184,6 +190,7 @@ function loadPreviewPhoto(i) {
     };
 }
 
+/*Reasigna los estilos de los botones de la 2ª barra de navegación para que el botón de la imagen que se esté viendo actualmente en el slider tenga un estilo diferente al resto.*/
 function changeNavLinks() {
     for (i = 0; i < imagesRoutes.length; i++) {
         nav2Links[i].style.backgroundColor = "red";
@@ -195,20 +202,21 @@ function changeNavLinks() {
             ///     EVENTOS     ///
             ///////////////////////
 
-window.addEventListener('load', relRoutes); // guardamos todas las rutas relativas al cargar la página.
-window.addEventListener('load', loadPreview);
+window.addEventListener('load', relRoutes); // guardamos todas las rutas relativas al cargar la página...
+window.addEventListener('load', loadPreview); // ... así como las imágenes de previsualización correspondientes.
 
-for (i = 0; i < canvasOpeners.length; i++) { // todos los enlaces para ampliar imágenes abrirán el slide.
+for (i = 0; i < canvasOpeners.length; i++) { // todos los enlaces para ampliar imágenes abrirán el slider.
     canvasOpeners[i].addEventListener('click', switchCanvas);
 }
 
-slideCloseButton.addEventListener('click', switchCanvas); // este botón cerrará el slide.
+slideCloseButton.addEventListener('click', switchCanvas); // este botón cerrará el slider.
 
-// se podrá navegar por el slide tanto hacia adelante como hacia atrás.
+// se podrá navegar por el slider tanto hacia adelante como hacia atrás.
 nav1Links[0].addEventListener("click", triggerSlider);
 nav1Links[1].addEventListener("click", triggerSlider);
 nav1Links[2].addEventListener("click", toggleSlider);
 
+// todos los botones de la 2ª barra de navegación del slider cargarán su foto correspondiente y activarán/desactivarán su imagen de previsualización correspondiente.
 for (i = 0; i < canvasOpeners.length; i++) {
     nav2Links[i].addEventListener('mouseover', activPreview(i));
     nav2Links[i].addEventListener('mouseout', deactivPreview(i));
